97f866174ea1c16b9450ea5c2b5d71585787fc60
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-01-21 06:37:35 +0000

    test: add tests for error handling

--- test/huggingface_dart_test.dart
+++ test/huggingface_dart_test.dart
@@ -4,24 +4,100 @@ import 'package:huggingface_dart/huggingface_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('gpt2', () async {
+  group('gpt2:', () {
     const input = 'Hello, my name is';
     const output = 'Hello, my name is John';
 
-    final api = HuggingFace.withClient(
-      model: 'gpt2',
-      outputType: HFOutputType.string,
-      apiToken: 'api_token',
-      client: MockClient((request) {
-        expect(request.url.host, 'api-inference.huggingface.co');
-        expect(request.url.path, '/models/gpt2');
-        expect(request.headers['Authorization'], 'Bearer api_token');
-        expect(request.body, input);
-        return Future.value(http.Response(output, 200));
-      }),
-    );
+    const error429 = '{"error":"TODO: Find an actual response"}';
+    const error404 = '{"error":"TODO: Find an actual response"}';
+    const error503 =
+        '{"error":"Model instruction-tuning-sd/cartoonizer is currently loading","estimated_time":219.25982666015625}';
+    const error400 =
+        '{"error":"cannot identify image file <_io.BytesIO object at 0x7f416c25bdb0>"}';
 
-    final result = await api.run<String>(input);
-    expect(result, output);
+    test('normal response', () async {
+      final api = HuggingFace.withClient(
+        model: 'gpt2',
+        outputType: HFOutputType.string,
+        apiToken: 'api_token',
+        client: MockClient((request) async {
+          expect(request.url.host, 'api-inference.huggingface.co');
+          expect(request.url.path, '/models/gpt2');
+          expect(request.headers['Authorization'], 'Bearer api_token');
+          expect(request.body, input);
+          return http.Response(output, 200);
+        }),
+      );
+
+      final result = await api.run<String>(input);
+      expect(result, output);
+    });
+
+    test('too many requests', () async {
+      final api = HuggingFace.withClient(
+        model: 'gpt2',
+        outputType: HFOutputType.string,
+        apiToken: 'api_token',
+        client: MockClient((request) async => http.Response(error429, 429)),
+      );
+
+      try {
+        await api.run<String>(input);
+        fail('Expected HuggingFaceTooManyRequestsError');
+      } on HuggingFaceTooManyRequestsError catch (e) {
+        expect(e.message, error429);
+      }
+    });
+
+    test('model not found', () async {
+      final api = HuggingFace.withClient(
+        model: 'gpt2',
+        outputType: HFOutputType.string,
+        apiToken: 'api_token',
+        client: MockClient((request) async => http.Response(error404, 404)),
+      );
+
+      try {
+        await api.run<String>(input);
+        fail('Expected HuggingFaceModelNotFoundError');
+      } on HuggingFaceModelNotFoundError catch (e) {
+        expect(e.message, error404);
+      }
+    });
+
+    test('model loading', () async {
+      final api = HuggingFace.withClient(
+        model: 'gpt2',
+        outputType: HFOutputType.string,
+        apiToken: 'api_token',
+        client: MockClient((request) async => http.Response(error503, 503)),
+      );
+
+      try {
+        await api.run<String>(input);
+        fail('Expected HuggingFaceLoadingError');
+      } on HuggingFaceLoadingError catch (e) {
+        expect(e.message,
+            'Model instruction-tuning-sd/cartoonizer is currently loading');
+        expect(e.estimatedTime, 219.25982666015625);
+      }
+    });
+
+    test('unknown error', () async {
+      final api = HuggingFace.withClient(
+        model: 'gpt2',
+        outputType: HFOutputType.string,
+        apiToken: 'api_token',
+        client: MockClient((request) async => http.Response(error400, 400)),
+      );
+
+      try {
+        await api.run<String>(input);
+        fail('Expected HuggingFaceUnknownError');
+      } on HuggingFaceUnknownError catch (e) {
+        expect(e.statusCode, 400);
+        expect(e.message, error400);
+      }
+    });
   });
 }
